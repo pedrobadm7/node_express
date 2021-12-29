40a55ee8a86349e5c6f91f13867c4b826b2c6470
pedrobadm7 <pedrobars7a@gmail.com>
2021-12-28 21:10:52 -0300

    configure eslint and editorconfig

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-    response.send('Hello World!');
-})
+  response.send('Hello World!');
+});
 
 app.listen(3000, () => console.log('🔥 Server started on port http://localhost:3000'));
