e8dcd89b6d45ccc6a2117db7da77c0db3306f14c
pedrobadm7 <pedrobars7a@gmail.com>
2021-12-30 21:01:04 -0300

    Tratando erros

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 
 const routes = require('./routes');
 
@@ -7,5 +8,12 @@ const app = express();
 app.use(express.json());
 
 app.use(routes);
+
+// Manipulador de erros
+app.use((error, request, response, next) => {
+  console.log('#### Error Handler');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('🔥 Server started on port http://localhost:3000'));
